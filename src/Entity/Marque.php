ff17ed92d1806401ac05a9a27cfbb73e2804eb64
abellenoue <@ssassin1397>
2020-08-19 15:47:03 +0200

    Marque

---
+++ src/Entity/Marque.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MarqueRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MarqueRepository::class)
+ */
+class Marque
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Produit::class, mappedBy="marque", orphanRemoval=true)
+     */
+    private $Produits;
+
+    public function __construct()
+    {
+        $this->Produits = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Produit[]
+     */
+    public function getProduits(): Collection
+    {
+        return $this->Produits;
+    }
+
+    public function addProduit(Produit $produit): self
+    {
+        if (!$this->Produits->contains($produit)) {
+            $this->Produits[] = $produit;
+            $produit->setMarque($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduit(Produit $produit): self
+    {
+        if ($this->Produits->contains($produit)) {
+            $this->Produits->removeElement($produit);
+            // set the owning side to null (unless already changed)
+            if ($produit->getMarque() === $this) {
+                $produit->setMarque(null);
+            }
+        }
+
+        return $this;
+    }
+}
